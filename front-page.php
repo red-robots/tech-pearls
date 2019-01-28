c6c2363ac42cd873b666a1361ebafb614af93014
lisadebona <annalisa.quiamco@gmail.com>
2019-01-28 21:03:41 +0800

    Updated front page

--- front-page.php
+++ front-page.php
@@ -149,62 +149,64 @@ get_header(); ?>
 			<?php } ?>
 
 
-			<?php
-				/* TESTIMONIAL SECTION & SUBSCRIBE FORM */
-				$args = array(
-					'posts_per_page'    => 8,
-				    'orderby'           => 'menu_order',
-				    'order'             => 'ASC',
-				    'post_type'         => 'testimonial',
-				    'post_status'       => 'publish'  
-			    );
-				$items = new WP_Query($args); ?>
-			<?php if ( $items->have_posts() ) { ?>
-				<section class="section section-testimonial clear">
-					<div class="wrapper">
-						<div class="testimonial column">
-							<div class="inside clear">
-								<div class="carousel-list">
-									<ul id="featuredTestimonial" class="list">
-										<?php while ( $items->have_posts() ) : $items->the_post();  ?>
-										<?php if( get_the_content() ) { ?>
-										<li class="testimonial-text">
-											<div class="text"><?php the_content(); ?></div>
-											<div class="author"><span class="author">- <?php the_title(); ?></span></div>
-										</li>
-										<?php } ?>
-										<?php endwhile; wp_reset_postdata(); ?>
-									</ul>
-								</div>
-								<div class="pager-nav"></div>
+		<?php
+		/* TESTIMONIAL SECTION & SUBSCRIBE FORM */
+		$args = array(
+			'posts_per_page'    => 8,
+		    'orderby'           => 'menu_order',
+		    'order'             => 'ASC',
+		    'post_type'         => 'testimonial',
+		    'post_status'       => 'publish'  
+	    );
+		$items = new WP_Query($args); ?>
+			
+		<section class="section section-testimonial clear">
+			<div class="wrapper">
+				<?php if ( $items->have_posts() ) { ?>
+					<div class="testimonial column">
+						<div class="inside clear">
+							<div class="carousel-list">
+								<ul id="featuredTestimonial" class="list">
+									<?php while ( $items->have_posts() ) : $items->the_post();  ?>
+									<?php if( get_the_content() ) { ?>
+									<li class="testimonial-text">
+										<div class="text"><?php the_content(); ?></div>
+										<div class="author"><span class="author">- <?php the_title(); ?></span></div>
+									</li>
+									<?php } ?>
+									<?php endwhile; wp_reset_postdata(); ?>
+								</ul>
 							</div>
+							<div class="pager-nav"></div>
 						</div>
-
-						<?php
-							$section_4_title = get_field('section_4_title');
-							$section_4_sub_title = get_field('section_4_sub_title');
-							$subscribe_form_shortcode = get_field('subscribe_form_shortcode');
-						?>
-						<?php if( $subscribe_form_shortcode && do_shortcode($subscribe_form_shortcode) ) { ?>
-						<div class="subcribe-form column">
-							<div class="inside clear">
-								<div class="form-wrapper clear">
-									<div class="titlediv clear">
-										<?php if($section_4_title) { ?>
-											<h2 class="formtitle1"><?php echo $section_4_title;?></h2>
-										<?php } ?>	
-										<?php if($section_4_sub_title) { ?>
-											<div class="formtitle2"><?php echo $section_4_sub_title;?></div>
-										<?php } ?>	
-									</div>
-									<?php echo do_shortcode($subscribe_form_shortcode); ?>
-								</div>
-							</div>
-						</div>
-						<?php } ?>
 					</div>
-				</section>
-			<?php } ?>
+				<?php } ?>
+
+				<?php
+					$section_4_title = get_field('section_4_title');
+					$section_4_sub_title = get_field('section_4_sub_title');
+					$subscribe_form_shortcode = get_field('subscribe_form_shortcode');
+				?>
+				<?php if( $subscribe_form_shortcode && do_shortcode($subscribe_form_shortcode) ) { ?>
+					<div class="subcribe-form column">
+						<div class="inside clear">
+							<div class="form-wrapper clear">
+								<div class="titlediv clear">
+									<?php if($section_4_title) { ?>
+										<h2 class="formtitle1"><?php echo $section_4_title;?></h2>
+									<?php } ?>	
+									<?php if($section_4_sub_title) { ?>
+										<div class="formtitle2"><?php echo $section_4_sub_title;?></div>
+									<?php } ?>	
+								</div>
+								<?php echo do_shortcode($subscribe_form_shortcode); ?>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
+		</section>
+			
 
 	<?php endwhile;  ?>
 </div><!-- #primary -->
